5edc3c831eaa4c05c15b20a4203e897a97c303e6
ITALO EMIDIO DA SILVA VASCONCELOS <ITALO0726@DIGITALCOLLEGE.LOCAL>
2024-08-14 11:42:39 -0300

    Adicionei o modelo de categoty no arquivo de models

--- src/models/models.js
+++ src/models/models.js
@@ -53,11 +53,88 @@ const Category = sequelize.define(
     },
 );
 
+const Product = sequelize.define(
+    'Product',
+    {
+        enabled: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        use_in_menu: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: 0
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        price_with_discount: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+
+    },
+);
+
+const ProdutoOpcao = sequelize.define('ProdutoOpcao', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Produtos', // Nome da tabela de produtos
+            key: 'id'
+        }
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    shape: {
+        type: DataTypes.ENUM('square', 'circle'),
+        defaultValue: 'square'
+    },
+    radius: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+    type: {
+        type: DataTypes.ENUM('text', 'color'),
+        defaultValue: 'text'
+    },
+    values: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
 // Sincronizar o modelo com o banco de dados    
 sequelize.sync();
 
-module.exports ={
+module.exports = {
     User,
     Category,
-    Product
+    Product,
 }
